059d6c3d516cada55d686f99668e0886ea93a83c
Fabri-Programmer <fabrizziopks@hotmail.com>
2024-09-12 10:05:33 -0500

    Primer commit 1

--- Hola1/Hola1/Program.cs
+++ Hola1/Hola1/Program.cs
@@ -10,6 +10,24 @@ while (limite < 100)
 */
 
 
+
+/*int respuesta;
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("QUE QUIERES INGRESAR = ");
+respuesta = int.Parse(Console.ReadLine());
+while(respuesta != 0)
+{
+    Console.Write("QUE QUIERES INGRESAR =  ");
+    respuesta = int.Parse(Console.ReadLine());
+    
+
+
+}
+Console.ForegroundColor =ConsoleColor.Green;
+Console.WriteLine("\nSE ACABO"); 
+*/
+
+
 /*int respuesta;
 Console.WriteLine("Desde donde quieres empezar = ");
 respuesta = int.Parse(Console.ReadLine());
@@ -21,7 +39,10 @@ while (respuesta > 0)
 */
 
 
-int respuesta;
+
+
+
+/* int respuesta;
 do
 {
     Console.WriteLine("¿Que numero de la tabla quieres que te muestre 1 ? = ");
@@ -46,10 +67,11 @@ while (respuesta3 < 13 && respuesta2 < 13 )
         respuesta3++;
     }
    
-}
-Console.WriteLine("AH FINALIZADO.. "); 
+} */
 
-/* int respuesta;
+//Console.WriteLine("AH FINALIZADO.. "); 
+
+int respuesta;
 do
 {
     Console.ForegroundColor = ConsoleColor.Red;
@@ -61,20 +83,31 @@ do
 int respuesta2;
 int suma = 0;
 Console.ForegroundColor = ConsoleColor.Blue;
-Console.WriteLine("\nIngresa un  numero = ");
+Console.WriteLine("\n !!   Okey perfecto    !! ");
+Console.WriteLine("Ingresa un numero y se sumara : ");
 respuesta2 = int.Parse(Console.ReadLine());
-while(respuesta2 >= 0)
+while(respuesta2 != 0)
 {
     suma = suma + respuesta2;
-    Console.WriteLine("\ningresa un numero = ");
+    Console.WriteLine("Ingresa un numero y se sumara : ");
     respuesta2 = int.Parse(Console.ReadLine());
 }
-
-Console.ForegroundColor = ConsoleColor.Green;
-Console.WriteLine("se acabo el programa");
-Console.ForegroundColor = ConsoleColor.Green;
+Console.ForegroundColor = ConsoleColor.DarkGreen;
 Console.WriteLine("Este es la suma de todos los pares e impares = "+suma);
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("\nFINALIZO EL PROGRAMA CRACK <3 !! ");
+
+
+/*for (int i = 4; i <= 16; i+=4)
+{
+    Console.Write("Hola = " +i );
+    
 */
+
+
+
+
+
 
 
 
--- Hola1/Hola1/obj/Debug/net6.0/Hola1.AssemblyInfo.cs
+++ Hola1/Hola1/obj/Debug/net6.0/Hola1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hola1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc025962f94445be9d8980344e2e5c9842fb0027")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hola1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hola1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
